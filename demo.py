7970af68057701142eb227dee4566c76a68aef01
AJkoder <jainanshi0306@gmail.com>
2026-03-22 21:05:02 +0530

    Modules

--- demo.py
+++ demo.py
@@ -1,18 +1,6 @@
-#File IO
-with open("notes.txt", "r") as f:
-    t = f.readlines()
-    print(t)
-    print("No of lines =", len(t))
+#modules
+import mymath
+import math
+print(mymath.Sum(2,3))
+print(math.sqrt(25))
 
-    c = 0
-    for i in t:
-        c += len(i.split())
-
-    print("No of words =", c)
-
-#Exception handling
-try:
-    with open("data.txt","r") as f:
-        print(f.read())
-except Exception as e:
-    print("Not found")
