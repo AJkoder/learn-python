2d32811fcc63a72210164643603a986bf0e040ca
AJkoder <jainanshi0306@gmail.com>
2026-04-02 18:56:59 +0530

    Creating POST request

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,45 +1,14 @@
-from flask import Flask, jsonify
-from flask import request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
+@app.route("/test", methods=["POST"])
+def test_post():
+    data = request.get_json()
+    
     return jsonify({
-        "status": "ok",
-        "message": "Hello"
+        "received": data
     })
-
-@app.route("/about")
-def about():
-    return jsonify({
-        "status": "success",
-        "message": "About Page"
-    })
-
-@app.route("/status")
-def status():
-    return jsonify({
-        "status": "success",
-        "message": "Server is running"
-    })
-
-@app.route("/api/info")
-def info():
-    return jsonify({
-        "name": "realchamp",
-        "phase": "Phase 3",
-        "goal": "Backend Developer"
-    })
-
-@app.route("/api/greet")
-def greet():
-    name = request.args.get("name", "Guest")
-
-    return jsonify({
-        "status": "success",
-        "message": f"Hello, {name}"
-    })
-
+ 
 if __name__ == "__main__":
     app.run(debug=True)
