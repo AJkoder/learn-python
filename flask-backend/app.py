29a5d7e435a413d7759bf82154ee67390b35f61e
AJkoder <jainanshi0306@gmail.com>
2026-04-03 12:24:55 +0530

    GET API

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,14 +1,40 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+tasks = []
 
-@app.route("/test", methods=["POST"])
-def test_post():
+@app.route("/tasks", methods=["POST"])
+def create_task():
     data = request.get_json()
-    
+
+    if data is None:
+        return jsonify({"error": "Invalid or missing JSON"}), 400
+
+    if not data:
+        return jsonify({"error": "Empty JSON provided"}), 400
+
+    if "title" not in data:
+        return jsonify({"error": "Title is required"}), 400
+
+    task = {
+        "id": len(tasks) + 1,
+        "title": data["title"],
+        "completed": False
+    }
+
+    tasks.append(task)
+
     return jsonify({
-        "received": data
-    })
+        "message": "Task created",
+        "task": task
+    }), 201
+
+@app.route("/tasks/<int:task_id>", methods=["GET"])
+def get_task(task_id):
+    for task in tasks:
+        if task["id"]==task_id:
+            return jsonify(task),200
+    return jsonify({"error":"task not found"}),404
  
 if __name__ == "__main__":
     app.run(debug=True)
